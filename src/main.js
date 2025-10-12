b6374bad56a78c74065cc70ceea0be922e854527
Rafael <gabriel.mito07@gmail.com>
2025-10-12 15:26:15 -0300

    Adicionando Mais uma Sessao: FAQ

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll('[data-tab-button');
+    const questions = document.querySelectorAll('[data-faq-question');
     
     // Funçao para Mudar as Sessoes ao Clicar
     for (let i = 0; i < buttons.length; i++) {
@@ -12,7 +13,19 @@ document.addEventListener("DOMContentLoaded", function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    // Função para mostrar a resposta ao clicar na pergunta
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 // Funçao para Mudar o Active dos Botoes
 function removeBotaoAtivo() {
