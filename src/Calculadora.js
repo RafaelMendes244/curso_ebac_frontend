41e45451cbb83b99063f742a517398f499bad5af
Rafael <gabriel.mito07@gmail.com>
2025-12-01 23:28:04 -0300

    Commit: Atividade MODULO26

---
+++ src/Calculadora.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso:
+console.log(multiplicar(5, 3));
+console.log(saudacao("Rafael"));
+//# sourceMappingURL=Calculadora.js.map
